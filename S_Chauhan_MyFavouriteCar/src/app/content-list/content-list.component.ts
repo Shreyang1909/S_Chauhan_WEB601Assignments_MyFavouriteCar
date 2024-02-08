2e1bd22e66cf4a212aa8a22a4591dd8ee710c8a7
VPatel0205 <124804245+VPatel0205@users.noreply.github.com>
2024-02-07 19:19:42 -0500

    Assignment 3 finished

--- S_Chauhan_MyFavouriteCar/src/app/content-list/content-list.component.ts
+++ S_Chauhan_MyFavouriteCar/src/app/content-list/content-list.component.ts
@@ -1,18 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
+import { ContentCardComponent } from '../content-card/content-card.component';
+import { TypegiverPipe } from '../typegiver.pipe';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-content-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ContentCardComponent, TypegiverPipe, FormsModule],
   templateUrl: './content-list.component.html',
   styleUrl: './content-list.component.scss'
 })
-export class ContentListComponent {
-  displayContentInfo(contentItem: Content) {
+export class ContentListComponent implements OnInit {
+  DisplayContentInformation(contentItem: Content) {
     console.log(`ID: ${contentItem.id} and Title: ${contentItem.title}`);
     }
   @Input () contentItems: Content[] = [];
+
+  searchTitle: string = '';
+  contentExists: boolean = false;
+  message: string = '';  
+  selectedTitle: string | null = null;
+
+  checkContentExists() {
+    const foundItem = this.contentItems.find(item => item.title.toLowerCase() === this.searchTitle.toLowerCase());
+    this.contentExists = !!foundItem;
+    this.message = foundItem ? 'Content item exists.' : 'Content item does not exist.';
+    this.selectedTitle = foundItem ? foundItem.title : null;
+  }
+
 
   ngOnInit(): void {
     this.contentItems =[
@@ -22,7 +39,7 @@ export class ContentListComponent {
         description:"The Ferrari SF90 Stradale is a high-performance plug-in hybrid hypercar",
         creator:"Ferrari",
         imgURL:"https://cdn.ferrari.com/cms/network/media/img/resize/5d1f10968a2538547edce275-m-dinamica-2400x4350-040719?width=768&height=1024",
-        type:"Hybrid Sports Car"
+        type:"Sports"
       },
       {
         id: 1,
@@ -30,14 +47,15 @@ export class ContentListComponent {
         description:"he Porsche 911 Carrera is an iconic sports car recognized for its timeless design",
         creator:"Porsche",
         imgURL:"https://files.porsche.com/filestore/image/multimedia/none/992-carrera-t-modelexplorer/normal/b34fb743-4ede-11ed-80f7-005056bbdc38;sS;twebp065/porsche-normal.webp",
-      },
+        type:"Sports"  
+       },
       {
         id: 2,
         title: "Tesla Model 3",
         description:"The Tesla Model 3 is an all-electric compact sedan",
         creator:"Tesla",
         imgURL:"https://cdn.carbuzz.com/car-thumbnails/700x375/15000/100/15123.jpg",
-        type:"Electri Sedan"
+        type:"Sedan"
       },
       {
         id: 3,
@@ -45,7 +63,7 @@ export class ContentListComponent {
         description: "The Chevrolet Bolt EV is a versatile all-electric hatchback.",
         creator: "Chevrolet",
         imgURL: "https://www.cnet.com/a/img/resize/a0a235f3c1f57107631e821ea1e5322df8af5855/hub/2020/09/21/7cfc6844-489d-441f-b782-3224a6b79d47/img-1498.jpg?auto=webp&width=1200",
-        type: "Electric Hatchback"
+        type: "Sports"
       },
       {
         id: 4,
@@ -53,7 +71,7 @@ export class ContentListComponent {
         description: "The Nissan Leaf is a popular all-electric compact car with advanced features.",
         creator: "Nissan",
         imgURL: "https://hips.hearstapps.com/hmg-prod/images/2023-nissan-leaf-9-1200x800-1649785072.jpg?crop=0.659xw:0.740xh;0.112xw,0.245xh&resize=768:*",
-        type: "Electric Compact Car"
+        type: "Sedan"
       },
       {
         id: 5,
@@ -61,8 +79,16 @@ export class ContentListComponent {
         description: "The BMW i4 is a stylish and high-performance all-electric sedan.",
         creator: "BMW",
         imgURL: "https://upload.wikimedia.org/wikipedia/commons/a/ad/BMW_i4_IMG_6695.jpg",
-        type: "Electric Sedan"
-      }
+        type: "Sports"
+      },
+      {
+        id: 6,
+        title: "Alto K10",
+        description: "Alto K10 VXI Specs & Features:Maruti Alto K10 VXI is a 5 seater Petrol car.Alto K10 VXI has Multi-function Steering Wheel, Touch Screen, Anti Lock Braking System, Power Windows Front, Wheel Covers, Passenger Airbag, Driver Airbag, Power Steering, Air Conditioner.",
+        creator: "Maruti",
+        imgURL: "https://auto.economictimes.indiatimes.com/files/retail_files/all-new-alto-k10-2022-1661145701-prod-var.jpg",
+        type: ""
+      },
       
     ];
   }
---
+++ S_Chauhan_MyFavouriteCar/src/app/typegiver.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TypegiverPipe } from './typegiver.pipe';
+
+describe('TypegiverPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TypegiverPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
